e670694db0a7cfa5b9182cb585c9ce81a1daa2af
Lukas Merz <lukemerz@gmail.com>
2025-08-19 10:13:02 +0200

    Add reference icon

--- src/components/sgc-icon/icons/crossSvg.ts
+++
@@ -1,5 +0,0 @@
-export const crossSvg = `
-  <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M4.20996 16.2898L16.7897 3.71009M16.7897 16.2898L4.21002 3.71008" stroke="currentColor" stroke-width="1.4" stroke-linecap="round"/>
-  </svg>
-`;
