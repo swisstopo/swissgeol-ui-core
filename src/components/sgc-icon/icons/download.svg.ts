49bd2f78e39a6867e837bc95452959183efcb17f
Lukas Merz <lukemerz@gmail.com>
2025-09-02 16:08:19 +0200

    Add missing icons

---
+++ src/components/sgc-icon/icons/download.svg.ts
@@ -0,0 +1,5 @@
+export const downloadSvg = `
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.99999 14.1667V2.5M9.99999 14.1667L4.99999 9.16667M9.99999 14.1667L15 9.16667M15.8333 17.5H4.16666" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`;
