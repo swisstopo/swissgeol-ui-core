50e11f0116eadf18241414e50f1d9138b0c67b7a
Lukas Merz <lukas.merz@ebp.ch>
2025-10-23 09:51:39 +0200

    Add navigation icons

---
+++ src/components/sgc-icon/icons/first.svg.ts
@@ -0,0 +1,5 @@
+export const firstSvg = `
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15 4.16732L9 10.0007L15 15.834M5 15.834L5 4.16732" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
